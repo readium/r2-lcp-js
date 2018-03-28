50f217347c797ccfe3246815a86675cbc0ea869e
danielweck <daniel.weck@gmail.com>
2018-03-28 15:34:30 +0100

    added license header in TypeScript source, see https://github.com/edrlab/readium-desktop/issues/105

--- src/parser/epub/lcp-certificate.ts
+++ src/parser/epub/lcp-certificate.ts
@@ -1,3 +1,10 @@
+// ==LICENSE-BEGIN==
+// Copyright 2017 European Digital Reading Lab. All rights reserved.
+// Licensed to the Readium Foundation under one or more contributor license agreements.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file exposed on Github (readium) in the project repository.
+// ==LICENSE-END==
+
 export const LCPCertificateBasicProfile = `-----BEGIN CERTIFICATE-----
 MIIFZzCCA0+gAwIBAgIJAMWdQaWkExQEMA0GCSqGSIb3DQEBCwUAMGcxCzAJBgNV
 BAYTAkZSMQ4wDAYDVQQHEwVQYXJpczEPMA0GA1UEChMGRURSTGFiMRIwEAYDVQQL
