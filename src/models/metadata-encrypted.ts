56c743682bab179369dc0f2ce3ad506c8d34441d
Daniel Weck <daniel.weck@gmail.com>
2019-01-17 15:19:36 +0000

    minor: code comments, references to JSON Schema

--- src/models/metadata-encrypted.ts
+++ src/models/metadata-encrypted.ts
@@ -11,21 +11,33 @@ import {
     JsonProperty,
 } from "ta-json-x";
 
+// tslint:disable-next-line:max-line-length
+// https://github.com/readium/webpub-manifest/blob/917c83e798e3eda42b3e9d0dc92f0fef31b16211/schema/extensions/epub/properties.schema.json#L56
 @JsonObject()
 export class Encrypted {
 
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/webpub-manifest/blob/917c83e798e3eda42b3e9d0dc92f0fef31b16211/schema/extensions/epub/properties.schema.json#L78
     @JsonProperty("scheme")
     public Scheme!: string;
 
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/webpub-manifest/blob/917c83e798e3eda42b3e9d0dc92f0fef31b16211/schema/extensions/epub/properties.schema.json#L73
     @JsonProperty("profile")
     public Profile!: string;
 
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/webpub-manifest/blob/917c83e798e3eda42b3e9d0dc92f0fef31b16211/schema/extensions/epub/properties.schema.json#L60
     @JsonProperty("algorithm")
     public Algorithm!: string;
 
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/webpub-manifest/blob/917c83e798e3eda42b3e9d0dc92f0fef31b16211/schema/extensions/epub/properties.schema.json#L65
     @JsonProperty("compression")
     public Compression!: string;
 
+    // tslint:disable-next-line:max-line-length
+    // https://github.com/readium/webpub-manifest/blob/917c83e798e3eda42b3e9d0dc92f0fef31b16211/schema/extensions/epub/properties.schema.json#L69
     @JsonProperty("original-length")
     public OriginalLength!: number;
 
